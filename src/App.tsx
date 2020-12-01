b2900fa4a82a694c90fe03bc9516abac578240e8
Daniel Cunha <danielcc.jp@gmail.com>
2020-12-01 12:40:50 -0300

    + configurando statusbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View, StatusBar } from 'react-native';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+    </>
+  );
 };
 
 export default App;
