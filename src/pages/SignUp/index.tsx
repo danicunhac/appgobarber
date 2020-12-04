4a4efb062ef655a1b7993e02849602d3f9b2d89f
Daniel Cunha <danielcc.jp@gmail.com>
2020-12-04 13:40:03 -0300

    + configurando navegação

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Text>SignUp</Text>
+    </Container>
+  );
+};
+
+export default SignUp;
