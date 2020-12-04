bc3a32383afdd99f8bc0fc65bf6daf90ff362344
Daniel Cunha <danielcc.jp@gmail.com>
2020-12-04 18:41:41 -0300

    + input & button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
@@ -11,6 +14,16 @@ const SignIn: React.FC = () => {
       <Image source={logoImg} />
 
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('Button');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
