23460c9052691f2de50b34c703c8a3f6deda7d5d
Daniel Cunha <danielcc.jp@gmail.com>
2020-12-10 13:47:54 -0300

    + autenticação de usuários

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -15,6 +15,8 @@ import * as Yup from 'yup';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
+import { useAuth } from '../../hooks/auth';
+
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
@@ -41,6 +43,8 @@ const SignIn: React.FC = () => {
 
   const navigation = useNavigation();
 
+  const { signIn, user } = useAuth();
+
   const handleSignIn = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
@@ -56,12 +60,12 @@ const SignIn: React.FC = () => {
         abortEarly: false,
       });
 
-      // await signIn({
-      //   email: data.email,
-      //   password: data.password,
-      // });
+      console.log(user);
 
-      // history.push('/dashboard');
+      await signIn({
+        email: data.email,
+        password: data.password,
+      });
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
